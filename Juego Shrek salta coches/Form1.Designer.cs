3689f72ecdbcd454001822c0798c4c0e6816b10a
IngMauVega <73960971+IngMauVega@users.noreply.github.com>
2020-11-04 13:11:28 -0600

    Add files via upload

---
+++ Juego Shrek salta coches/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Juego_Shrek_salta_coches
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.chuek = new System.Windows.Forms.PictureBox();
+            this.Carretera = new System.Windows.Forms.PictureBox();
+            this.BtnReset = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chuek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carretera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnReset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // chuek
+            // 
+            this.chuek.BackColor = System.Drawing.Color.Transparent;
+            this.chuek.Image = global::Juego_Shrek_salta_coches.Properties.Resources.Shrek;
+            this.chuek.Location = new System.Drawing.Point(25, 240);
+            this.chuek.Name = "chuek";
+            this.chuek.Size = new System.Drawing.Size(61, 56);
+            this.chuek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.chuek.TabIndex = 3;
+            this.chuek.TabStop = false;
+            // 
+            // Carretera
+            // 
+            this.Carretera.Image = ((System.Drawing.Image)(resources.GetObject("Carretera.Image")));
+            this.Carretera.Location = new System.Drawing.Point(-12, 298);
+            this.Carretera.Name = "Carretera";
+            this.Carretera.Size = new System.Drawing.Size(931, 10);
+            this.Carretera.TabIndex = 2;
+            this.Carretera.TabStop = false;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.BackColor = System.Drawing.Color.Transparent;
+            this.BtnReset.Image = ((System.Drawing.Image)(resources.GetObject("BtnReset.Image")));
+            this.BtnReset.Location = new System.Drawing.Point(222, 57);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(84, 49);
+            this.BtnReset.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BtnReset.TabIndex = 4;
+            this.BtnReset.TabStop = false;
+            this.BtnReset.Visible = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 359);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.Carretera);
+            this.Controls.Add(this.chuek);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.chuek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carretera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnReset)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox chuek;
+        private System.Windows.Forms.PictureBox Carretera;
+        private System.Windows.Forms.PictureBox BtnReset;
+    }
+}
+
---
+++ Juego Shrek salta coches/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Juego_Shrek_salta_coches
+{
+    public partial class Form1 : Form
+    {
+        int PosicionInicialPisoY = 296;
+        int PosicionInicialpisoX = 5;
+        int velocidad = 5;
+        bool brincochuek;
+        PictureBox cocheIzq = new PictureBox();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void iniciarjuego()
+        {
+
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == Convert.ToChar(Keys.Space)))
+            {
+                brincochuek = true;
+            }        
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int pospiso;
+            pospiso = Carretera.Location.X - 5;
+            Carretera.Location = new Point(pospiso, PosicionInicialPisoY);
+            if (pospiso < -130)
+            {
+                pospiso = PosicionInicialpisoX;
+                Carretera.Location = new Point(PosicionInicialpisoX, PosicionInicialPisoY);
+            }
+           
+        }
+        public void generarcoche()
+        {
+
+            PictureBox coche2 = new PictureBox();
+            coche2.Image = Image.FromFile(Application.StartupPath + (@"\IMG\vocho.jpg"));
+            coche2.Location = new Point(430, 260);
+            coche2.BackColor = Color.Transparent;
+            coche2.SizeMode = PictureBoxSizeMode.Zoom;            
+            cocheIzq = coche2;
+            this.Controls.Add(coche2);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int lx = cocheIzq.Location.X;
+            int ly = cocheIzq.Location.Y;
+            this.Controls.Remove(cocheIzq);
+            lx = lx - 15;
+            PictureBox coche2 = new PictureBox();
+            coche2.Image = Image.FromFile(Application.StartupPath + (@"\IMG\vocho.jpg"));
+            coche2.Location = new Point(lx, ly);
+            coche2.BackColor = Color.Transparent;
+            coche2.SizeMode = PictureBoxSizeMode.StretchImage;
+            cocheIzq = coche2;
+            this.Controls.Add(coche2);
+            // int puntuacion = Convert.ToInt32(lblpuntos.Text);
+            if (coche2.Location.X < 0)
+            {
+                this.Controls.Remove(coche2);
+                timer2.Interval = timer2.Interval - 5;
+                if (timer2.Interval<=5)
+                {
+                    timer2.Interval = 8;
+                }
+                //puntuacion = puntuacion + 1;
+                generarcoche();
+            }
+            if (chuek.Bounds.IntersectsWith(coche2.Bounds))
+                {
+                timer1.Stop(); timer2.Stop(); timer3.Stop();this.Controls.Remove(chuek);
+                SoundPlayer player = new SoundPlayer();
+                player.SoundLocation = Application.StartupPath + (@"\sonidos\uh.wav");
+                player.Play();
+                BtnReset.Visible = true;
+            }
+
+            }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (brincochuek)
+            {
+                int Brinco = chuek.Location.Y - 20;
+                chuek.Location = new Point(chuek.Location.X, Brinco);
+                if (Brinco <= 160)
+                {
+                    brincochuek = false;
+                }
+            }
+            else
+            {
+                if (chuek.Location.Y < 250)
+                {
+                    int Brinco = chuek.Location.Y + 10;
+                    chuek.Location = new Point(chuek.Location.X, Brinco);
+
+                }
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            iniciarjuego();
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+        }
+    }
+    }
+
